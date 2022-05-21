e4c79cb9479a53c303f821f83d20ead0b9419778
Lenovo <d.shparko@mail.ru>
2022-05-21 08:45:43 +0300

    add auth

--- server/index.js
+++ server/index.js
@@ -2,24 +2,24 @@ const express = require("express")
 const mongoose = require("mongoose")
 const config = require("config")
 const authRouter = require("./routes/auth.routes")
-
 const app = express()
 const PORT = config.get('serverPort')
+const corsMiddleware = require('./middleware/cors.middleware')
 
-
+app.use(corsMiddleware)
 app.use(express.json())
-app.use("/api/auth",authRouter)
+app.use("/api/auth", authRouter)
 
 
-const start=async ()=> {
+const start = async () => {
     try {
-
-
-      //  console.log(config.get('dbUrl'))
-        await mongoose.connect(config.get("dbUrl"))
+        await mongoose.connect(config.get("dbUrl"), {
+            useNewUrlParser:true,
+            useUnifiedTopology:true
+        })
 
         app.listen(PORT, () => {
-            console.log("server started on port", PORT)
+            console.log('Server started on port ', PORT)
         })
     } catch (e) {
         console.log(e)
--- src/client/pages/auth_page/auth_page.js
+++ src/client/pages/auth_page/auth_page.js
@@ -1,16 +1,18 @@
-import React from 'react'
-
+import React, {useState} from 'react';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import {Grid, Paper,  TextField, Button, Typography,Link} from '@material-ui/core'
+import {registration} from "../../../actions/user";
 const Login =({handleChange})=>{
     const paperStyle={padding :20,height: 664,width:340, margin:"0 auto"}
 
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
     const btnstyle={margin:'8px 0'}
     return(
         <Grid>
             <Paper  style={paperStyle}>
-                <TextField label='Username' placeholder='Enter username' fullWidth required/>
+                <TextField value = {email} label='Username' placeholder='Enter username' fullWidth required/>
                 <TextField label='Password' placeholder='Enter password' type='password' fullWidth required/>
                 <FormControlLabel
                     control={
@@ -28,7 +30,7 @@ const Login =({handleChange})=>{
                     </Link>
                 </Typography>
                 <Typography > Do you have an account ?
-                    <Link href="#" onClick={()=>handleChange("event",1)} >
+                    <Link href="#" onClick={()=>registration(email,password)} >
                         Sign Up
                     </Link>
                 </Typography>
