739638f07f544753d218c7edbc53f5c55a9f04f8
darya-cherr <chernikovadarya67@gmail.com>
2022-05-27 01:40:22 +0300

    Fix game position

--- src/client/pages/game/index.js
+++ src/client/pages/game/index.js
@@ -2,13 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import Header from "../../Components/Header";
 const Game = () => {
 
-return(
-ReactDOM.render(
+return(<>
+
+
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-))}
+)</>)}
     export default Game;
